ce66746d626b63389c73d84ebc1e1e1d0b1092bf
Joao Francisco <joaofrancisco21@yahoo.com.br>
2023-07-21 00:53:49 -0300

    Alterado: Som agora é um arquivo local para evitar possíveis falhas

--- js/sounds.js
+++ js/sounds.js
@@ -1,10 +1,6 @@
 export default function () {
-  const buttonPressAudio = new Audio(
-    "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true"
-  );
-  const kitchenTimer = new Audio(
-    "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true"
-  );
+  const buttonPressAudio = new Audio("./assets/sounds/button-press.wav");
+  const kitchenTimer = new Audio("./assets/sounds/kichen-timer.mp3");
   const forestSound = new Audio("./assets/sounds/Floresta.wav");
   const rainSound = new Audio("./assets/sounds/Chuva.wav");
   const fireplaceSound = new Audio("./assets/sounds/Lareira.wav");
